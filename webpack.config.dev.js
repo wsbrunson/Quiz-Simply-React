3ec2937b14e67ad23b40055c461ac3b9f3c5c716
wsbrunson <brunson.ws@gmail.com>
2016-03-05 16:21:06 -0500

    Fixed tests, added AirBnB eslint config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,35 +1,37 @@
+/* eslint-disable no-var */
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint-enable no-var */
 
 module.exports = {
-	devtool: 'cheap-eval-source-map',
+	devtool: 'inline-source-map',
 
 	entry: [
 		'webpack-dev-server/client?http://localhost:8080',
 		'webpack/hot/dev-server',
-		'./src/js/main'
+		'./src/js/main',
 	],
 
 	output: {
 		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js'
+		filename: 'bundle.js',
 	},
 
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(),
 		new HtmlWebpackPlugin({
-			template: './src/index.html'
+			template: './src/index.html',
 		}),
 		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify('development')
-		})
+			'process.env.NODE_ENV': JSON.stringify('development'),
+		}),
 	],
 
 	module: {
 		loaders: [{
 			test: /\.scss$/,
-			loaders: ['style', 'css', 'sass']
+			loaders: ['style', 'css', 'sass'],
 		}, {
 			test: /\.js$/,
 			exclude: /node_modules\/(?!(stardust))/,
@@ -37,16 +39,16 @@ module.exports = {
 			query: {
 				cacheDirectory: true,
 				plugins: [
-					'transform-decorators-legacy'
+					'transform-decorators-legacy',
 				],
-				presets: ['es2015', 'react', 'stage-2']
-			}
-		}]
+				presets: ['es2015', 'react', 'stage-2'],
+			},
+		}],
 	},
 
 	devServer: {
 		contentBase: './dist',
 		// hot: true,
-		historyApiFallback: true
-	}
+		historyApiFallback: true,
+	},
 };
