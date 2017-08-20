b629286be3105f4dce6eb068cf1c4a784b8396a8
wsbrunson <brunson.ws@gmail.com>
2017-08-20 13:36:18 -0500

    remove old code

--- src/js/components/NoMatch.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const NoMatch = () => <div>Page Not found</div>;
-
-export default NoMatch;
--- src/js/configureStore.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import { createStore, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk";
-
-import rootReducer from "./reducers/rootReducer";
-
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
-
-export default store;
